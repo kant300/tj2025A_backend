171473bde84802add17bd4c194ea4bdd58b04ebe
kant300 <p.o.i.u.0987552@gmail.com>
2025-07-10 10:25:25 +0900

    생성자 이론정리

--- src/day08/Member.java
+++ src/day08/Member.java
@@ -2,11 +2,11 @@ package day08;
 
 public class Member {
     //1. 멤버변수
-    String id = "guest";
+    String id ;
     boolean isLogin = false;
     // 2.기본생성자
     Member() {
-        System.out.println("--> Member 기본생성자 실행");
-
+        id = "guest";
+        isLogin = false;
     }   //func e
 } //class e
