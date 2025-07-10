9133a01793163393a0397264f62a34b7f8fe97b1
kant300 <p.o.i.u.0987552@gmail.com>
2025-07-10 11:42:45 +0900

    접근제한자

---
+++ src/day09/Example2.java
@@ -0,0 +1,14 @@
+package day09;
+
+public class Example2 {
+    public static void main(String[] args) {
+        /*
+        [ final ]
+            1. 수정불가능한/고정상태를 정의할때 사용하는 키워드
+
+        [ static ] *정적인
+            1. 실행중에 변화가 없는 상태를 정의할때 사용되는 키워드
+
+         */
+    }
+}
