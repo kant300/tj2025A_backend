e1f80f1b05e3497029f91e3366fd894d8c33c4c8
kant300 <p.o.i.u.0987552@gmail.com>
2025-07-21 17:20:12 +0900

    실습13-미해결

--- src/day15/실습13.java
+++ src/day15/실습13.java
@@ -1,12 +1,12 @@
 package day15;
 
 // [1]
-interface Soundable{
+interface Soundable{ // 인터페이스 주요 멤버 : 상수,  추상메소드 : 구현부{} 없는 메소드
     // 인터페이스 주요 멤버 : 상수 , 추상메소드 : 구현부{} 없는 메소드
-    void makeSound();
+    public abstract void makeSound();   // public abstract 생략 가능
 }
 class Cat implements Soundable{
-    // 오버라이딩 자동완성 :
+    // 오버라이딩 자동완성 : 클래스 안에서 컨트롤+스페이바 -> 방향키 위로 -> 선택후 엔터
     @Override
     public void makeSound() { System.out.println("야옹"); }
 }
@@ -24,12 +24,12 @@ interface RemoteControl{
 public class 실습13 {
     public static void main(String[] args) {
         // [1]
-        new Cat().makeSound();
-        Soundable sound = new Dog(); sound.makeSound();
+        new Cat().makeSound();   // 야옹
+        Soundable sound = new Dog(); sound.makeSound(); // 멍멍
 
         // [2]
-        System.out.println( RemoteControl.MAX_VOLUME );
-        System.out.println( RemoteControl.MIN_VOLUME );
+        System.out.println( RemoteControl.MAX_VOLUME ); // 10
+        System.out.println( RemoteControl.MIN_VOLUME ); // 0
 
 
     }// main e
